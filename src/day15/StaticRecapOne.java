4b73b0f8a6bea5db033625c4763a2d758e57e1e1
chels <chels@10.0.0.213>
2022-01-12 22:28:07 -0500

    Day15 hw

---
+++ src/day15/StaticRecapOne.java
@@ -0,0 +1,70 @@
+package day15;
+
+public class StaticRecapOne {
+
+	public static boolean isJavaFun;
+	String month;
+	
+	public static int totalAmount;
+	String iLikeJava;
+	
+	String country;
+	int age;
+	char gender;
+	public static double temperature;
+
+	public void setCountry() {
+
+		//TODO call [country] variable and assign value = "USA";
+		country = "USA";
+
+		//TODO  print the value of variable [country]
+		System.out.println(country);
+	}
+
+	public void getCountry() {
+
+		
+		// call method [setCountry()]
+		 setCountry(); {
+			
+		}
+		
+
+		// call variable [gender] and assign value 'f'
+		gender = 'f';
+
+		// print the [gender] variable
+		System.out.println(gender);
+
+		// call variable[age] and assign value 11
+		age = 11;
+
+		// print variable [age]
+		System.out.println(age);
+	}
+
+	public static void addNum(int a, int b) {
+		int result = a+b;	
+		System.out.println(result);
+		
+		
+		//TODO call [temperature] variable and assign value = 55.55;
+		StaticRecapOne.temperature = 55.55;
+		//TODO  print the value of variable [temperature]
+		System.out.println(StaticRecapOne.temperature);
+		
+		
+		 
+	}
+
+	public static void divNum(int a, int b) {
+
+
+		//TODO  call method[addNum()]
+		divNum (10, 2);
+		System.out.println(a / b);
+		
+
+	}
+}
---
+++ src/day15/StaticRecapTwo.java
@@ -0,0 +1,45 @@
+package day15;
+
+public class StaticRecapTwo {
+
+	// static method
+		public static void getInfo() {
+
+			//TODO call [isJavaFun] variable and assign value = true
+			
+
+			//TODO call [totalAmount] variable and assign value = 1111
+
+			//TODO print value of variable [isJavaFun]
+
+			//TODO print value of variable [totalAmount]
+
+			//TODO call method [addNum()]
+
+	 
+
+		}
+
+		// non-static method
+		public void statusUpdate() {
+
+			//TODO call [month] variable and assign value = "January"
+
+			//TODO call [iLikeJava] variable and assign value = "Java is life"
+
+			//TODO print value of variable [month]
+
+			//TODO print value of variable [iLikeJava]
+
+			//TODO call method [getCountry()]
+
+
+
+		}
+
+		public static void main(String[] args) {
+			 
+
+			//TODO call method [getInfo()]
+		}
+	}

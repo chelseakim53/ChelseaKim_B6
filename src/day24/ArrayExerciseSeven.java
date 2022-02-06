1582e7d1cf20581f57dcfd4f71b0d5b73cfd1cc1
chels <chels@10.0.0.213>
2022-02-06 13:34:15 -0500

    day24 hw

--- src/day24/ArrayExerciseSeven.java
+++ src/day24/ArrayExerciseSeven.java
@@ -1,34 +1,47 @@
 package day24;
 
 public class ArrayExerciseSeven {
-	
-	public static int getIndexNumber (int [] number, int elementvalue) {
-		int result = 0;
-		
-		
-		
-		
+
+	public static void main(String[] args) {
+
+		int n1[] = { 88, 22, 6, 1, 2, 3, 88, 22, 44, 33 };
+		int resultOne = getIndexNumber(n1, 3);
+		System.out.println(resultOne);
+
+		int n2[] = { 1, 2, 3, 22, 44, 33 };
+		int resultTwo = getIndexNumber(n2, 12);
+		System.out.println(resultTwo);
+
+		int n3[] = { 88, 22, 6, 1, 8, 0 };
+		int resultThree = getIndexNumber(n3, 22);
+		System.out.println(resultThree);
+	}
+
+	public static int getIndexNumber(int[] number, int elementValue) {
+		int result = -1;
+
+		for (int i = 0; i < number.length; i++) {
+			if (number[i] == elementValue) {
+				result = i;
+				break;
+			}
+		}
+
 		return result;
+
 	}
 
 }
 
-
 /**
-7. Write a method that takes 2 parameters. An array of int parameter and integer
-parameter. Find the index number of an array element. Check if an array has the
-element value passed as 2nd parameter. If it contains return the index number of that
-element otherwise return -1.
-
-Access Modifier: public
-Non-Access Modifier: static
-Return Type: int
-Method Name: getIndexNumber
-Parameter1: int [] number
-Parameter1: int elementValue
-Test Data:
-getIndexNumber ([88,22,6, 1, 2, 3, 88,22,44,33], 3) ----- 5
-getIndexNumber ([1, 2, 3,22,44,33], 12) ----- -1
-getIndexNumber ([88,22,6, 1,8,0], 22) ----- 1
-getIndexNumber ([2, 3, 88,22,44], 44) ----- 4
-*/
+ * 7. Write a method that takes 2 parameters. An array of int parameter and
+ * integer parameter. Find the index number of an array element. Check if an
+ * array has the element value passed as 2nd parameter. If it contains return
+ * the index number of that element otherwise return -1.
+ * 
+ * Access Modifier: public Non-Access Modifier: static Return Type: int Method
+ * Name: getIndexNumber Parameter1: int [] number Parameter1: int elementValue
+ * Test Data: getIndexNumber ([88,22,6, 1, 2, 3, 88,22,44,33], 3) ----- 5
+ * getIndexNumber ([1, 2, 3,22,44,33], 12) ----- -1 getIndexNumber ([88,22,6,
+ * 1,8,0], 22) ----- 1 getIndexNumber ([2, 3, 88,22,44], 44) ----- 4
+ */
--- src/day24/ArrayExerciseSix.java
+++ src/day24/ArrayExerciseSix.java
@@ -1,5 +1,45 @@
 package day24;
 
 public class ArrayExerciseSix {
+	
+	public static void main(String[] args) {
+
+		int n1[] = { 6, 1, 2, 3 };
+		int n2[] = { 13, 2, 3, 4, 6, 1, 2, 3 };
+		int n3[] = { 13, 5, 3, 4, 5, 1, 2, 3 };
+
+		boolean resultOne = checkNum(n1);
+
+		boolean resultTwo = checkNum(n2);
+
+		boolean resultThree = checkNum(n3);
+
+		System.out.println(resultOne);// false
+		System.out.println(resultTwo);// true
+		System.out.println(resultThree);// true
+	}
+
+	protected static boolean checkNum(int[] number) {
+		boolean result = false;
+
+		int fiveCount = 0;// keep track of number 5 occurrence in an array
+		int twoCount = 0;// keep track of number 2 occurrence in an array
+
+		for (int i = 9; 0 < number.length; i++) {
+			if (number[i] == 2) {
+
+				twoCount++;
+
+			} else if (number[i] == 5) {
+				fiveCount++;
+			}
+		}
+
+		// check if value 2 or value 5 number of appearance in array is 2
+		if (twoCount == 2 || fiveCount == 2) {
+			result = true;
+		}
+		return result;
+	}
 
 }
